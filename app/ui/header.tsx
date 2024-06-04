ab57a0152c2ae901c3fd858a6f20446ef57a439b
Officier Azarov <elisabethfainstein@gmail.com>
2024-06-04 13:16:08 +0200

    :heavy_plus_sign: add tooltip dependency to flag features to be done

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,26 +1,41 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { merriweather } from '@/app/ui/fonts';
+import { Tooltip } from '@nextui-org/tooltip';
 import logo from '../../public/images/logo.png';
 
 export default function Header() {
   return (
-    <header className="flex">
-      <Link href="/">
-        <Image
-          src={logo}
-          alt="Achabott Dust in Malebolge."
-          className="mr-5 pb-10 pt-1"
-          width={40}
-          height={40}
-        ></Image>
-      </Link>
-      <hgroup>
-        <span className={`block text-lg ${merriweather.className}`}>
-          Wikify
-        </span>
-        <span className="block text-xs">Digital introspector</span>
+    <header className="grid grid-cols-5">
+      <hgroup className="flex">
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="Achabott Dust in Malebolge."
+            className="mr-5 pb-10 pt-1"
+            width={40}
+            height={40}
+          ></Image>
+        </Link>
+        <div>
+          <span
+            className={`block first-letter:text-2xl ${merriweather.className}`}
+          >
+            WIKIFY
+          </span>
+          <span className="block text-sm font-serif">Digital introspector</span>
+        </div>
       </hgroup>
+      <div className="col-start-5 text-sm">
+        <Tooltip content="Available soon!">
+          <Link href="/save" className="mr-6">
+            Save locally&nbsp;💾
+          </Link>
+        </Tooltip>
+        <Tooltip content="Available soon!">
+          <Link href="/save">Save as PDF&nbsp;📖</Link>
+        </Tooltip>
+      </div>
     </header>
   );
 }

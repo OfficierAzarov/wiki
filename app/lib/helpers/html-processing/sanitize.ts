fbd9f92e63d28ae1b2fa16e2c503b3a7d25def05
Officier Azarov <elisabethfainstein@gmail.com>
2024-07-25 18:25:11 +0200

    :recycle: change DOMPurify to Isomorphic DOMPurify

--- app/lib/helpers/html-processing/sanitize.ts
+++ app/lib/helpers/html-processing/sanitize.ts
@@ -1,5 +1,5 @@
 import { UntrustedHTML } from '../../../types/html';
-import DOMPurify from 'dompurify';
+import DOMPurify from 'isomorphic-dompurify';
 
 export const sanitize = (unsanitizedHtml: UntrustedHTML): string => {
   return DOMPurify.sanitize(unsanitizedHtml);

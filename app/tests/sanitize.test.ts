f863ace028920742b701e55705d500f5720e2bcd
officierazarov <elisabethfainstein@gmail.com>
2024-08-11 19:15:45 +0200

    :white_check_mark: add sanitisation tests

--- app/tests/sanitize.test.ts
+++ app/tests/sanitize.test.ts
@@ -1,30 +1,27 @@
-// import { sanitize } from '../lib/helpers/html-processing/sanitize';
-// import { UntrustedHTML } from '../types/html';
+import { sanitize } from '../lib/helpers/html-processing/sanitize';
+import { UntrustedHTML } from '../types/html';
 
-// describe('sanitize', () => {
-//   it('should remove script tags from the input HTML', () => {
-//     const unsanitizedHtml: UntrustedHTML =
-//       "<div onclick='alert(1)'>Click me</div><script>alert('XSS')</script>";
-//     const sanitizedHtml = sanitize(unsanitizedHtml);
-//     expect(sanitizedHtml).not.toContain('<script>');
-//     expect(sanitizedHtml).toContain('<div>Click me</div>');
-//   });
+describe('sanitize', () => {
+  it('should remove script tags from the input HTML', () => {
+    const unsanitizedHtml: UntrustedHTML =
+      "<div onclick='alert(1)'>Click me</div><script>alert('XSS')</script>";
+    const sanitizedHtml = sanitize(unsanitizedHtml);
+    expect(sanitizedHtml).not.toContain('<script>');
+    expect(sanitizedHtml).toContain('<div>Click me</div>');
+  });
 
-//   it('should allow safe HTML tags', () => {
-//     const unsanitizedHtml: UntrustedHTML = '<b>Bold</b><i>Italic</i>';
-//     const sanitizedHtml = sanitize(unsanitizedHtml);
-//     expect(sanitizedHtml).toContain('<b>Bold</b>');
-//     expect(sanitizedHtml).toContain('<i>Italic</i>');
-//   });
+  it('should allow safe HTML tags', () => {
+    const unsanitizedHtml: UntrustedHTML = '<b>Bold</b><i>Italic</i>';
+    const sanitizedHtml = sanitize(unsanitizedHtml);
+    expect(sanitizedHtml).toContain('<b>Bold</b>');
+    expect(sanitizedHtml).toContain('<i>Italic</i>');
+  });
 
-//   it('should remove dangerous attributes', () => {
-//     const unsanitizedHtml: UntrustedHTML =
-//       '<img src="image.jpg" onerror="alert(1)" />';
-//     const sanitizedHtml = sanitize(unsanitizedHtml);
-//     expect(sanitizedHtml).toContain('<img src="image.jpg"');
-//     expect(sanitizedHtml).not.toContain('onerror');
-//   });
-// });
-// function expect(sanitizedHtml: string) {
-//   throw new Error('Function not implemented.');
-// }
+  it('should remove dangerous attributes', () => {
+    const unsanitizedHtml: UntrustedHTML =
+      '<img src="image.jpg" onerror="alert(1)" />';
+    const sanitizedHtml = sanitize(unsanitizedHtml);
+    expect(sanitizedHtml).toContain('<img src="image.jpg"');
+    expect(sanitizedHtml).not.toContain('onerror');
+  });
+});
